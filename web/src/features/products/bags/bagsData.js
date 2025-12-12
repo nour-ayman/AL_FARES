de26db6f62a7c9379c2b730798e379f6fa763f61
Wezza <nouraymanix@gmail.com>
2025-12-12 20:43:37 +0200

    feat(ui): Integrate retro font and update styles across bags, tools, and textbooks sections

--- web/src/features/products/bags/bagsData.js
+++ web/src/features/products/bags/bagsData.js
@@ -1,24 +1,24 @@
 const bagsData = [
   {
-    id: 1,
+    id: 5,
     name: "Classic School Backpack",
     image: "/images/bag1.png",
     price: 180,
   },
   {
-    id: 2,
+    id: 6,
     name: "Sporty Laptop Bag",
     image: "/images/bag2.png",
     price: 350,
   },
   {
-    id: 3,
+    id: 7,
     name: "Casual Shoulder Bag",
     image: "/images/bag3.png",
     price: 220,
   },
   {
-    id: 4,
+    id: 8,
     name: "Leather College Bag",
     image: "/images/bag4.png",
     price: 400,
--- web/src/features/products/tools/toolsData.js
+++ web/src/features/products/tools/toolsData.js
@@ -1,24 +1,24 @@
 const toolsData = [
   {
-    id: 1,
+    id: 9,
     name: "Geometry Set",
     image: "/images/tools1.png",
     price: 55,
   },
   {
-    id: 2,
+    id: 10,
     name: "Scientific Calculator",
     image: "/images/tools2.png",
     price: 320,
   },
   {
-    id: 3,
+    id: 11,
     name: "Highlighters Pack",
     image: "/images/tools3.png",
     price: 40,
   },
   {
-    id: 4,
+    id: 12,
     name: "Drawing Compass",
     image: "/images/tools4.png",
     price: 25,
